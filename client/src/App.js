620057f418e7f5b737e4ac060d172d297dd7a207
Patrick Ulmer <patrick.ulmer@campoint.net>
2021-01-08 18:02:53 +0100

    fix and clean up App

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,19 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <Link className="App-link" to="/">Home</Link>
-          <Link className="App-link" to="/otherpage">Other Page</Link>
-        </header>
-        <div className="Container">
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
-        </div>
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link className="App-link" to="/">Home</Link>
+        <Link className="App-link" to="/otherpage">Other Page</Link>
+      </header>
+      <div className="Container">
+        <Route exact path="/" component={Fib} />
+        <Route path="/otherpage" component={OtherPage} />
       </div>
-    </Router>
-  );
-}
+    </div>
+  </Router>
+);
 
 export default App;
